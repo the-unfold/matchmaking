019d60afeb69b9c81d7222432e1359ac76963118
vv90 <vv90@mechmind.net>
2020-04-11 23:17:42 +0700

    initial setup

---
+++ src/App.fs
@@ -0,0 +1,35 @@
+module App
+
+open Browser.Dom
+open Fable.Core
+open Fable.React
+open Fable.React.Helpers
+
+[<Import("Map", from="ol")>]
+type Map()=
+    class
+    end
+
+[<Import("View", from="ol")>]
+type View()=
+    class
+    end
+
+[<Import("TileLayer", from="ol/layer/Tile")>]
+type TileLayer()=
+    class
+    end
+
+[<Import("OSM", from="ol/source/OSM")>]
+type OSM()=
+    class
+    end
+
+let initMap()=
+    let map: Map = Map()
+
+let init()=
+    let element = str "root"
+    ReactDom.render (element, document.getElementById "root")
+    
+init()
